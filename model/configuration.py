39aaae949ad6f20511f0859c6bb48ea4781bcc91
Kaan Bilgili <kaan.bilgili@std.izmirekonomi.edu.tr>
2026-05-16 17:47:04 +0300

    fix on configuation and skip compilation for interprated languages
    add is_interpreted field to Configuration and  skip compilation for interpreted languages

--- model/configuration.py
+++ model/configuration.py
@@ -8,6 +8,7 @@ class Configuration:
     source_filename: str = "main.c"
     compiler_args: str = "-o main.exe"
     run_command: str = "./main.exe"
+    is_interpreted: bool = False
 
     def to_dict(self) -> dict:
         """Converts the configuration to a dict for JSON storage."""
@@ -17,6 +18,7 @@ class Configuration:
             "source_filename": self.source_filename,
             "compiler_args": self.compiler_args,
             "run_command": self.run_command,
+            "is_interpreted": self.is_interpreted,
         }
 
     @staticmethod
@@ -28,4 +30,5 @@ class Configuration:
             source_filename=data.get("source_filename", "main.c"),
             compiler_args=data.get("compiler_args", "-o main.exe"),
             run_command=data.get("run_command", "./main.exe"),
+            is_interpreted=data.get("is_interpreted", False),
         )
