f450903db90c2bc8afdd1544c3c1359bf9cd469f
Simge Gökalp <simge.gokalp@std.izmirekonomi.edu.tr>
2026-05-28 17:46:46 +0300

    Update zip_processor.py
    Implemented ZIP processing for batch student submissions, including extraction, folder creation, and error handling for invalid ZIP files.

--- service/zip_processor.py
+++ service/zip_processor.py
@@ -1,5 +1,6 @@
 import os
 import zipfile
+import shutil
 
 
 class ZipProcessor:
@@ -35,6 +36,9 @@ class ZipProcessor:
 
         folder_path = os.path.join(self.submissions_folder, student_id)
 
+        if os.path.exists(folder_path):
+            shutil.rmtree(folder_path)
+
         os.makedirs(folder_path, exist_ok=True)
 
         return folder_path
@@ -44,22 +48,52 @@ class ZipProcessor:
 
         zip_files = self.get_zip_files()
 
+        results = [] 
+        
         for zip_file in zip_files:
-
-            student_id = zip_file.replace(".zip", "")
-
-            zip_path = os.path.join(self.submissions_folder, zip_file)
-
-            try:
-
-                extract_folder = self.create_student_folder(student_id)
-
-                self.extract_zip(zip_path, extract_folder)
-
-                print(f"{student_id} processed successfully.")
-
-            except Exception as e:
-
-                print(f"Error processing {student_id}: {e}")
-
-                continue 
+            student_id = os.path.splitext(zip_file)[0] 
+            zip_path = os.path.join(self.submissions_folder, zip_file) 
+            try: 
+                extract_folder = self.create_student_folder(student_id) 
+                self.extract_zip(zip_path, extract_folder) 
+                result = {
+                     "student_id": student_id, 
+                     "status": "SUCCESS", 
+                     "path": extract_folder 
+                } 
+                
+                results.append(result) 
+                print(f"{student_id} processed successfully.") 
+            except zipfile.BadZipFile: 
+                result = {
+                    "student_id": student_id, 
+                    "status": "BAD_ZIP", 
+                    "path": None 
+                } 
+                
+                results.append(result) 
+                
+                print(f"{student_id} contains an invalid ZIP file.") 
+                
+            except FileNotFoundError:
+                result = {
+                    "student_id": student_id, 
+                    "status": "FILE_NOT_FOUND", 
+                    "path": None 
+                } 
+                
+                results.append(result) 
+                print(f"{student_id} file could not be found.") 
+                
+            except Exception as e: 
+                result = {
+                    "student_id": student_id, 
+                    "status": "ERROR", 
+                    "error": str(e), 
+                    "path": None 
+                } 
+                
+                results.append(result) 
+                print(f"Error processing {student_id}: {e}") 
+                 
+        return results
